06f91a5c7a56ed050545c47f0b4b60d3164cd0eb
Jan <58750565+ThatJanGuy@users.noreply.github.com>
2022-09-19 15:46:50 +0200

    Add ToTitle()

--- LittleHelpers/TextManipulation.cs
+++ LittleHelpers/TextManipulation.cs
@@ -46,5 +46,42 @@
             Console.WriteLine(heading);
             Console.WriteLine(string.Concat(Enumerable.Repeat<char>(lineChar, heading.Length)));
         }
+
+
+        /// <summary>
+        /// Accepts string and returns it with the first letter of every word capitalised.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns></returns>
+
+        public static string ToTitle(string input)
+        {
+            input = input.Trim().ToLower();
+            List<string> words = new();
+            string output = "";
+
+            foreach(string word in input.Split(' '))
+            {
+                if (word.Trim().Length == 0)
+                {
+                    continue;
+                }
+                else if (word.Trim().Length == 1)
+                {
+                    words.Add(word.ToUpper());
+                }
+                else
+                {
+                    words.Add(word[0].ToString().ToUpper() + word.Substring(1));
+                }
+            }
+
+            foreach (string word in words)
+            {
+                output += word + " ";
+            }
+
+            return output.Trim();
+        }
     }
 }
